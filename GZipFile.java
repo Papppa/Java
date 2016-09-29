d06fd85c58a9d4dc9474d738757cbbe858038715
André Hoppe <andre-hoppe7@gmx.de>
2016-09-29 16:28:49 +0200

    Add files via upload

--- GZipFile.java
+++ GZipFile.java
@@ -1,21 +1,26 @@
 package Implements;
 
+import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 import Implements.CSVReader;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.zip.GZIPInputStream;
 import Implements.FileElmts;
 
 public class GZipFile 
 {
-	private static final String INPUT_GZIP_FILE = "data/1.gz";
-    private static final String OUTPUT_FILE = "data/10.csv";
+	private static final String INPUT_GZIP_FILE = "data/2016-01-01-03-59-59_Birmingham_Germany_TTI_External.csv.gz";
+    private static final String OUTPUT_FILE = "data/2016-01-01-03-59-59_Birmingham_Germany_TTI_External.csv";
     
     public static void main(String[] args )
     {
     	GZipFile gZip = new GZipFile();
     	gZip.gunzipIt();
+    	
     }
     
     public void gunzipIt()
@@ -29,21 +34,51 @@ public class GZipFile
 
             	 FileOutputStream out =
                     new FileOutputStream(OUTPUT_FILE);
+            	 
 
-                int len = gzis.read(buffer);
+
+                int len;
                 
-                System.out.println(buffer);
-                
-                /*
-                while (len > 0)
+                while ((len = gzis.read(buffer)) > 0)
                 {
                 	out.write(buffer, 0, len);
                 }
+                
+                BufferedReader br = null;
+        		String line = "";
+        		String cvsSplitBy = ",";
+        		String x;
+        		int y;
+        		
+         		try{
+        			br = new BufferedReader(new FileReader(OUTPUT_FILE));
+        			while ((line = br.readLine()) != null) {
+        				String[] content = line.split(cvsSplitBy);
+        				String[] intContent = line.split(cvsSplitBy);
+        				x = content[0];
+        				y = Integer.parseInt(intContent[1]);
+
+        				System.out.println((y));				
+        			}
+        			
+        		} catch(FileNotFoundException e) {
+        			e.printStackTrace();
+        		} catch (IOException e) {
+        			e.printStackTrace();
+        		} finally {
+        			if (br != null) {
+        				try {
+        					br.close();
+        				} catch (IOException e) {
+        					e.printStackTrace();
+        				}
+        			}
+        		}
+                
                 gzis.close();
             	out.close();
 
             	System.out.println("Done");
-            	*/
             	
         }
         catch(IOException ex)
