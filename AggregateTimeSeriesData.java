97edffd6735b29a8d1249a045786a1ddc1ead231
André Hoppe <andre-hoppe7@gmx.de>
2016-11-17 17:09:44 +0100

    Add files via upload
    -runs quiet perfect.
    -bugs needed to fix: 
    can't parse the full database. Could be empty arrays in some datafiles...
    --> skip empty arrays 
    --> name timestamps where entrys are empty

--- AggregateTimeSeriesData.java
+++ AggregateTimeSeriesData.java
@@ -1,8 +1,10 @@
 package Implements;
 
 import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -11,23 +13,28 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.io.FileWriter;
 
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.lang3.StringUtils;
 
 public class AggregateTimeSeriesData {
 
 	private static final int COLUMN_TIMESTAMP = 0;
 	private static final String fileSource = "jamdataExample/";
+	private static final String finalCSVFile = "tsfile.csv";
 	
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		// TODO start and stop time
 		Instant startTime = Instant.parse("2015-01-01T16:00:02Z"); // Iso Format 2016-01-01T03:00:00Z
 		Instant stopTime= Instant.parse("2015-01-01T17:59:41Z");
 		List<TrafficJamDataPoint> tsData = loadTrafficJamData(startTime, stopTime);
 		File path = null;
-		writeToFile(path, tsData);
+		writeTSFile(tsData);
 		
 	}
 
@@ -35,7 +42,7 @@ public class AggregateTimeSeriesData {
 		// TODO initialize cleverly by calculating the number of minutes (*2)
 		List<TrafficJamDataPoint> tsData = new ArrayList<>(200);
 		List<File> filesToRead = new ArrayList<>();
-		for (File file : filesToRead) {
+		for (File file : getAllFiles(startTime,stopTime)) {
 			loadData(file, startTime, stopTime, tsData);
 		}
 		return tsData;
@@ -46,7 +53,6 @@ public class AggregateTimeSeriesData {
 		// startTime.isBefore(arg)
 		List<File> fileList = new ArrayList<File>();
 		fileList = getAllFiles(startTime,stopTime);
-		List<String> aggTsData = new ArrayList<>();
 		String line ="";
 		BufferedReader br = null;
 		for (int j=0; j<fileList.size();j++){
@@ -62,13 +68,10 @@ public class AggregateTimeSeriesData {
 						Instant time = Instant.parse(content[COLUMN_TIMESTAMP].substring(0, 20));
 						dp = new TrafficJamDataPoint(time);
 						dp.addDataLine(content);
-						aggTsData.addAll(Arrays.asList(String.valueOf(dp.getTime()),String.valueOf(dp.getTotalDelay()),String.valueOf(dp.getTotalLength()),String.valueOf(dp.getCount())));
+						tsData.add(dp);
 					} 
 					else if (content[COLUMN_TIMESTAMP].isEmpty()) {
 						dp.addDataLine(content);
-						aggTsData.set(aggTsData.size()-3, String.valueOf(dp.getTotalDelay()));
-						aggTsData.set(aggTsData.size()-2, String.valueOf(dp.getTotalLength()));
-						aggTsData.set(aggTsData.size()-1, String.valueOf(dp.getCount()));
 					}
 				}
 			}
@@ -87,8 +90,18 @@ public class AggregateTimeSeriesData {
 		}
 		return filePaths;
 	}
-	private static void writeToFile(File path, List<TrafficJamDataPoint> tsData){
-		// write method for writing a new csv with the aggregated timeseries data
-	}
-
+	
+	public static void writeTSFile (List<TrafficJamDataPoint> tsData) throws Exception {
+		FileWriter writer = new FileWriter(finalCSVFile);
+		for(int i=0;i<tsData.size(); i++){
+			String ts = tsData.get(i).getTime().toString();
+			String length = String.valueOf(tsData.get(i).getTotalLength());
+			String delay = String.valueOf(tsData.get(i).getTotalDelay());
+			String count = String.valueOf(tsData.get(i).getCount());
+			CSVUtils.writeLine(writer, Arrays.asList(ts,length,delay,count),',');
+		}
+		System.out.println("CSV File printed succesfully");
+		writer.flush();
+		writer.close();
+		}
 }
